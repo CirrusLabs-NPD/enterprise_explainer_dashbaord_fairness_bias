ce3429d528d8483fd19c3e45d92c82d42478f71d
Aakash bhardwaj <112869168+Aakash-bhardwaj-cirruslabs@users.noreply.github.com>
2025-07-23 12:20:33 +0530

    added deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/enterprise_explainer_dashbaord_fairness_bias',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
