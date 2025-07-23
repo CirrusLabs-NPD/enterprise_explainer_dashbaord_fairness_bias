15a248beebb6e0438d98ad673892ca215e8a549f
Aakash bhardwaj <112869168+Aakash-bhardwaj-cirruslabs@users.noreply.github.com>
2025-07-23 16:08:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/enterprise_explainer_dashbaord_fairness_bias/',
+  base: '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
